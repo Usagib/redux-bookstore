e5e2d433f2cdb40c343cddd98e69f144639e6e9c
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-07-21 19:02:25 +0200

    created the componets folder and files, reducer folder

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BookList from './BookList'
+import BookForm from './BookForm'
+
+const App = () => {
+    return (
+        <div>
+            <BookList />
+            <BookForm />
+        </div>
+    )
+}
+
+export default App;
