acc74e764bc98bd5ca72a28cf56ef08b1db442e7
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-07-21 11:43:29 -0500

    Linters fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'; // eslint-disable-line no-unused-vars
+import ReactDOM from 'react-dom'; // eslint-disable-line no-unused-vars
 // import PropTypes from 'prop-types';
