fafdc5d38900d1c1b19beab0ea3f0f34b57cd336
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-07-23 18:08:43 +0200

    fix for the books.js reducer for removing a book

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -6,7 +6,7 @@ const booksReducer = (state = [], action) => {
     case CREATE_BOOK:
       return [...state, action.book];
     case REMOVE_BOOK:
-      return true;
+      return state.slice(0, action.id).concat(state.slice(action.id + 1));
     default:
       return state;
   }
