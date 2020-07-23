35b234ee27eb946ca2d3eb00fe1038cc0591d76c
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-07-23 17:50:26 +0200

    add the initial state in index.js

--- src/index.js
+++ src/index.js
@@ -7,10 +7,29 @@ import rootReducer from "./reducers/index";
 // import PropTypes from 'prop-types';
 import App from "./components/App";
 
-const store = createStore(rootReducer);
+const initialState = [
+  {
+    id: Math.round(Math.random() * 100),
+    title: "Tom Swayer",
+    category: "Kids",
+  },
+  {
+    id: Math.round(Math.random() * 100),
+    title: "The Crow",
+    category: "Horror",
+  },
+  {
+    id: Math.round(Math.random() * 100),
+    title: "Tiger Tiger",
+    category: "Sci-Fi",
+  },
+];
+
+const store = createStore(rootReducer, { books: initialState });
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById("root")
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,27 +1,7 @@
 const CREATE_BOOK = "CREATE_BOOK";
 const REMOVE_BOOK = "REMOVE_BOOK";
 
-const initialState = {
-  books: [
-    {
-      id: Math.round(Math.random() * 100),
-      title: "Tom Swayer",
-      category: "Kids",
-    },
-    {
-      id: Math.round(Math.random() * 100),
-      title: "The Crow",
-      category: "Horror",
-    },
-    {
-      id: Math.round(Math.random() * 100),
-      title: "Tiger Tiger",
-      category: "Sci-Fi",
-    },
-  ],
-};
-
-const booksReducer = (state = initialState, action) => {
+const booksReducer = (state = [], action) => {
   switch (action.type) {
     case CREATE_BOOK:
       return [...state, action.book];
