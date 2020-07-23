89640b2f4b1e07699023ac778f6cfa7f5ed4533b
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-07-23 13:28:59 +0200

    .

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
 const REMOVE_BOOK = "REMOVE_BOOK";
 const CREATE_BOOK = "CREATE_BOOK";
 
-const createBook = (book) => ({ type: CREATE_BOOK, book: book });
+const createBook = (book) => ({ type: CREATE_BOOK, book: book })
 
-const removeBook = (book) => ({ type: REMOVE_BOOK, book: book });
+const removeBook = (book) => ({ type: REMOVE_BOOK, book: book })
 
 export {
   createBook,
