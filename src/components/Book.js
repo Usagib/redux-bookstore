ba6fa185bac117be109e72c5db2bcf24312cb8ef
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-07-28 17:58:21 +0200

    created the click handler

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,17 +4,25 @@ import PropTypes from "prop-types";
 const Book = (props) => {
   const { book } = props;
   const { id, title, category } = book;
+  const { clickHandler } = props;
+  const removeBook = () => {
+    clickHandler(book);
+  };
   return (
-    <tr key={`book${id}`}>
+    <tr>
       <td>{id}</td>
       <td>{title}</td>
       <td>{category}</td>
+      <td><button type="submit" onClick={() => {
+        removeBook(book)
+      }}>Remove</button></td>
     </tr>
   );
 };
 
 Book.propTypes = {
-  book: PropTypes.string.isRequired,
+  id: PropTypes.string,
+  title: PropTypes.string,
 };
 
 export default Book;
