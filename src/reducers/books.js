b1d4a7d2b4c57ceced20aca032ab17faf0996f8c
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-07-28 17:57:16 +0200

    added the initial state

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,15 +1,34 @@
 const CREATE_BOOK = "CREATE_BOOK";
 const REMOVE_BOOK = "REMOVE_BOOK";
 
-const booksReducer = (state = [], action) => {
+const initialState = [
+  {
+    id: Math.round(Math.random() * 100),
+    title: "Tom Swayer",
+    category: "Kids",
+  },
+  {
+    id: Math.round(Math.random() * 100),
+    title: "The Crow",
+    category: "Horror",
+  },
+  {
+    id: Math.round(Math.random() * 100),
+    title: "Tiger Tiger",
+    category: "Sci-Fi",
+  },
+];
+
+const bookReducer = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_BOOK:
       return [...state, action.book];
     case REMOVE_BOOK:
-      return state.slice(0, action.id).concat(state.slice(action.id + 1));
+      console.log(action);
+      return state.filter(book => book.id !== action.book.id);
     default:
       return state;
   }
 };
 
-export default booksReducer;
+export default bookReducer;
