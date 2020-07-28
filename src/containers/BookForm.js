00faec6995df1b271d916d1aa14b0140aedc0faa
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-07-28 10:47:56 -0500

    BookForm changed to stateful component
    implemented handlechange and handle submit, dispatched method to props

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { createBook } from '../actions/index';
 
 const cat = [
   "Action",
@@ -10,18 +12,75 @@ const cat = [
   "Sci-Fi",
 ];
 
-const BookForm = () => (
-  <form>
-    <h3>Add a book</h3>
-    <br />
-    <input type="text" id="Title" placeholder="title" />
-    <br />
-    <select id="Category">
-      <option value={cat}>Category</option>
-      {cat.map((cat) => (<option key={cat.id}>{cat}</option>))}
-    </select>
-    <button type="submit">Confirm</button>
-  </form>
-);
+class BookForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: Math.round(Math.random()*100),
+      title: '',
+      category: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-export default BookForm;
+  handleChange(event) {
+    event.preventDefault();
+    switch(event.target.id) {
+      case 'Category':
+        this.setState({
+          category: event.target.value,
+        });
+      break;
+      case 'Title':
+        this.setState({
+          title: event.target.value,
+        });
+      break;
+      default:
+       return this.state;
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.bookSubmit(this.state);
+    this.setState({
+      id: Math.round(Math.random()*100),
+      title: '',
+      category: '',
+    });
+  }
+
+  render() {
+    return(
+      <form>
+        <h3>Add a book</h3>
+        <br />
+        <input id="Title"
+          value={this.state.title}
+          onChange={this.handleChange}
+          type="text"
+          placeholder="title" />
+        <br />
+        <select id="Category"
+          value={this.state.category}
+          onChange={this.handleChange}>
+          <option key="cat-default" value={cat}>Category</option>
+          {cat.map(cat => (<option key={`cat-${cat}`}>{cat}</option>))}
+        </select>
+        <button type="submit" onClick={this.handleSubmit}>Confirm</button>
+      </form>
+    );
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    bookSubmit: (book) => {
+      dispatch(createBook(book))
+    }
+  }
+};
+
+export default connect(null, mapDispatchToProps)(BookForm);
