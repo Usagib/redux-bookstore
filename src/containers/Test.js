9f671da3c57aab0a9201975eeecc82b9132c40f4
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-08-04 04:05:46 -0500

    frontend test finished

--- src/containers/Test.js
+++ src/containers/Test.js
@@ -10,7 +10,11 @@ const Test = props => {
         <div className="nav-list">
           <ul className="nav-list-items">
             <li className="list-item">BOOKS</li>
-            <li className="list-item">CATEGORIES</li>
+            <li className="list-item">
+              <select className="nav-select">
+                <option>CATEGORY</option>
+              </select>
+            </li>
           </ul>
         </div>
         <div className="nav-buttons">
@@ -29,21 +33,88 @@ const Test = props => {
           </ul>
         </div>
         <div className="lesson-progress">
-          <div className="box">
-            <div className="percent">
-              <svg>
-                <circle cx="70" cy="70" r="70"></circle>
-                <circle cx="70" cy="70" r="70"></circle>
-              </svg>
-              <div className="number">
-                <h2>87<span>%</span></h2>
-              </div>
-            </div>
-            <h2 class="text">Completed</h2>
-          </div>
+          <svg>
+            <circle cx="35" cy="35" r="35"></circle>
+            <circle cx="35" cy="35" r="35"></circle>
+          </svg>
+          <p className="number">87%</p>
+          <p className="completed">Completed</p>
         </div>
         <div className="lesson-update">
+          <p className="update-current">CurrentExample</p>
+          <p className="update-chapter">ChapterExample</p>
+          <button className="update-button">Update Progress</button>
         </div>
+      </div>
+      <div className="lesson-panel">
+        <div className="lesson-description">
+          <p className="lesson-description-category">ExampleFiction</p>
+          <p className="lesson-description-title">ExampleDune</p>
+          <p className="lesson-description-author">Exampleauthor</p>
+          <ul className="lesson-description-links">
+            <li className="lesson-link">Comments</li>
+            <li className="lesson-link">Remove</li>
+            <li className="lesson-link">Edit</li>
+          </ul>
+        </div>
+        <div className="lesson-progress">
+          <svg>
+            <circle cx="35" cy="35" r="35"></circle>
+            <circle cx="35" cy="35" r="35"></circle>
+          </svg>
+          <p className="number">87%</p>
+          <p className="completed">Completed</p>
+        </div>
+        <div className="lesson-update">
+          <p className="update-current">CurrentExample</p>
+          <p className="update-chapter">ChapterExample</p>
+          <button className="update-button">Update Progress</button>
+        </div>
+      </div>
+      <div className="lesson-panel">
+        <div className="lesson-description">
+          <p className="lesson-description-category">ExampleFiction</p>
+          <p className="lesson-description-title">ExampleDune</p>
+          <p className="lesson-description-author">Exampleauthor</p>
+          <ul className="lesson-description-links">
+            <li className="lesson-link">Comments</li>
+            <li className="lesson-link">Remove</li>
+            <li className="lesson-link">Edit</li>
+          </ul>
+        </div>
+        <div className="lesson-progress">
+          <svg>
+            <circle cx="35" cy="35" r="35"></circle>
+            <circle cx="35" cy="35" r="35"></circle>
+          </svg>
+          <p className="number">87%</p>
+          <p className="completed">Completed</p>
+        </div>
+        <div className="lesson-update">
+          <p className="update-current">CurrentExample</p>
+          <p className="update-chapter">ChapterExample</p>
+          <button className="update-button">Update Progress</button>
+        </div>
+      </div>
+      <div className="form">
+        <hr className="line-div"></hr>
+        <p className="form-title">ADD NEW BOOK</p>
+        <form className="form">
+          <input
+            type="text"
+            placeholder="Book title"
+            className="input-title"
+          />
+        <select className="input-select">
+          <option>Category</option>
+        </select>
+        <button
+          type="submit"
+          className="input-submit"
+        >
+        ADD BOOK
+        </button>
+        </form>
       </div>
     </div>
 
